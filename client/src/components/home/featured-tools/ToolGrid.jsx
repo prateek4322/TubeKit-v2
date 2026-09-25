9200e5bfd3b505959e6d37a05f3e31c38908dcb5
Prateek Kumar <prateek4322@gmail.com>
2026-09-25 10:57:59 +0530

    Update ToolGrid.jsx

--- client/src/components/home/featured-tools/ToolGrid.jsx
+++ client/src/components/home/featured-tools/ToolGrid.jsx
@@ -3,7 +3,20 @@ import { featuredTools } from "@/data/featuredTools";
 
 function ToolGrid() {
   return (
-    <div className="grid gap-8 md:grid-cols-2 xl:grid-cols-3">
+    <div
+      className="
+        grid
+        grid-cols-1
+        gap-x-5
+        gap-y-7
+        sm:grid-cols-2
+        sm:gap-x-6
+        sm:gap-y-8
+        lg:grid-cols-3
+        lg:gap-x-7
+        lg:gap-y-9
+      "
+    >
       {featuredTools.map((tool) => (
         <ToolCard key={tool.id} tool={tool} />
       ))}
