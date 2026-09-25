d13fbcaf5166a3449ed91291b4db6fd1d6f1130a
Prateek Kumar <prateek4322@gmail.com>
2026-09-25 10:12:47 +0530

    Create essentialTools.js

---
+++ client/src/data/essentialTools.js
@@ -0,0 +1,50 @@
+export const essentialTools = [
+  {
+    id: 1,
+    icon: "💰",
+    title: "Money Calculator",
+    path: "/tools/money-calculator",
+  },
+  {
+    id: 2,
+    icon: "📈",
+    title: "Monetization Checker",
+    path: "/tools/monetization-checker",
+  },
+  {
+    id: 3,
+    icon: "💵",
+    title: "CPM Calculator",
+    path: "/tools/cpm-calculator",
+  },
+  {
+    id: 4,
+    icon: "💹",
+    title: "RPM Calculator",
+    path: "/tools/rpm-calculator",
+  },
+  {
+    id: 5,
+    icon: "🆔",
+    title: "Channel ID Finder",
+    path: "/tools/channel-id-finder",
+  },
+  {
+    id: 6,
+    icon: "🎥",
+    title: "Video ID Extractor",
+    path: "/tools/video-id-extractor",
+  },
+  {
+    id: 7,
+    icon: "🖼️",
+    title: "Thumbnail Downloader",
+    path: "/tools/thumbnail-downloader",
+  },
+  {
+    id: 8,
+    icon: "🏷️",
+    title: "YouTube Tag Extractor",
+    path: "/tools/tag-extractor",
+  },
+];
